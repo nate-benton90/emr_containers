20c882298d7d857ddfd64d4d9775d70f539e6919
nate-benton90 <nbenton90@gmail.com>
2024-05-10 08:08:44 -0500

    added eks setup to be later populated

---
+++ infra/eks/cluster.tf
@@ -0,0 +1,24 @@
+resource "aws_eks_cluster" "example" {
+  name     = "minimal-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnet[*].id
+  }
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.eks_subnet[*].id
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+  instance_types = ["t3.micro"]
+  capacity_type = "SPOT"
+  labels = {
+    usage = "test-cluster"
+  }
+}
