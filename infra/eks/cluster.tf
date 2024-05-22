ab70d7d1e522f6df77dbabb10b50674647a82f1c
nate-benton90 <nbenton90@gmail.com>
2024-05-22 10:49:49 -0500

    all resources here apply and destroy successfully. note the local use of ssh. now need to setup simple spark jobs and iam config to run them

--- infra/eks/cluster.tf
+++ infra/eks/cluster.tf
@@ -1,4 +1,3 @@
-// Variables for cluster creation
 variable "eks_vpc_id" {
   description = "The ID of the VPC"
   type        = string
@@ -19,7 +18,6 @@ variable "node_role_arn" {
   type        = string
 }
 
-// Cluster provisioning/config
 resource "aws_eks_cluster" "foo-emr-eks-cluster" {
   name     = "foo-emr-eks-cluster"
   role_arn = var.role_arn
@@ -28,21 +26,62 @@ resource "aws_eks_cluster" "foo-emr-eks-cluster" {
   }
 }
 
-// Create EC2 node group for EKS
+resource "aws_security_group" "eks_node_group_sg" {
+  name_prefix = "eks-node-group-"
+  vpc_id      = var.eks_vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-node-group-sg"
+  }
+}
+
+# NOTE: Adjust the path to your SSH public key
+resource "aws_key_pair" "eks_key_pair" {
+  key_name   = "eks_key_pair"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_eks_node_group" "emr-eks-node-group-foo" {
   depends_on = [aws_eks_cluster.foo-emr-eks-cluster]
   cluster_name    = aws_eks_cluster.foo-emr-eks-cluster.name
   node_group_name = "emr-eks-node-group"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.eks_subnet_ids
+
   scaling_config {
     desired_size = 1
     max_size     = 1
     min_size     = 1
   }
+
   instance_types = ["t3.micro"]
+  ami_type = "AL2_x86_64"
   capacity_type = "SPOT"
+  
   labels = {
     usage = "test-cluster"
+  }
+
+  remote_access {
+    ec2_ssh_key = aws_key_pair.eks_key_pair.key_name
+    source_security_group_ids = [aws_security_group.eks_node_group_sg.id]
+  }
+  
+  tags = {
+    Name = "emr-eks-node-group"
   }
 }
--- infra/vpc/vpc.tf
+++ infra/vpc/vpc.tf
@@ -6,6 +6,24 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "eks_igw"
+  }
+}
+
+resource "aws_route_table" "eks_public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+  tags = {
+    Name = "eks_public_rt"
+  }
+}
+
 resource "aws_subnet" "eks_subnet" {
   count = 2
   vpc_id            = aws_vpc.eks_vpc.id
@@ -15,7 +33,13 @@ resource "aws_subnet" "eks_subnet" {
   tags = {
     Name = "eks_subnet_${count.index}"
     "kubernetes.io/cluster/foo-emr-eks-cluster" = "shared"
-    }
+  }
+}
+
+resource "aws_route_table_association" "eks_subnet_association" {
+  count = 2
+  subnet_id      = element(aws_subnet.eks_subnet[*].id, count.index)
+  route_table_id = aws_route_table.eks_public_rt.id
 }
 
 output "eks_vpc_id" {
