275958cc44ce4e66678737e8c73662b85638b684
nate-benton90 <nbenton90@gmail.com>
2024-12-25 15:14:22 -0600

    lambda added and now trying to start emr container job which is gonna be a bitch for iam - forget the container part lol

--- infra/lambda/functions.tf
+++ infra/lambda/functions.tf
@@ -8,43 +8,31 @@ variable "lambda_execution_role_name" {
 // Resources
 resource "null_resource" "lambda_package" {
   triggers = {
-    script_sha = filesha256("./infra/lambda/runtimes/config_emr_eks_job.py")
+    always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "powershell.exe ./packager.ps1"
+    command = <<EOF
+    powershell.exe -File ./infra/lambda/runtimes/packager.ps1
+  EOF
   }
 }
 
-# TODO: refactor paths and change location of data usage to another section
-data "archive_file" "zip_the_python_code" {
-  type        = "zip"
-  source_dir  = "./infra/lambda/runtimes/config_emr_eks_job.py"
-  output_path = "./infra/lambda/runtimes/packages/config_emr_eks_job.zip"
-  depends_on = [
-    null_resource.lambda_package
-  ]
-}
-
-resource "aws_lambda_layer_version" "lambda_layer" {
-  filename = "layer.zip"
-  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  layer_name = "emr_container_layer"
-  compatible_runtimes = ["python3.10"]
-  depends_on = [
-    data.archive_file.zip_the_python_code
-  ]
-}
+# resource "aws_lambda_layer_version" "lambda_layer" {
+#   filename = "layer.zip"
+#   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+#   layer_name = "emr_container_layer"
+#   compatible_runtimes = ["python3.10"]
+#   depends_on = [
+#     data.archive_file.zip_the_python_code
+#   ]
+# }
 
 # TODO: also, refactor paths
 resource "aws_lambda_function" "start_emr_container_job" {
-  filename         = "./infra/lambda/runtimes/packages/lambda_function_payload.zip"
+  filename         = "./infra/lambda/runtimes/lambda_function_payload.zip"
   function_name    = "start_emr_container_job"
   role             = var.lambda_execution_role_name
   handler          = "config_emr_eks_job.main"
   runtime          = "python3.10"
-  layers = [
-    aws_lambda_layer_version.lambda_layer.arn
-  ]
-  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  depends_on = [data.archive_file.zip_the_python_code]
+  depends_on = [null_resource.lambda_package]
 }
--- infra/lambda/runtimes/config_emr_eks_job.py
+++ infra/lambda/runtimes/config_emr_eks_job.py
@@ -6,50 +6,158 @@ import logging
 
 # ---------------------------------------------------------------------------------------------
 
-# NOTE: setup logging
+# NOTE: setup global logging
 log = logging.getLogger(__name__)
+logging.basicConfig(level = logging.DEBUG)
 log_handler = logging.StreamHandler()
 log_handler.setFormatter(logging.Formatter(fmt='%(asctime)s  %(levelname)s  %(message)s',
-                                        datefmt='%Y-%m-%d %H:%M:%S'))
+                                           datefmt='%Y-%m-%d %H:%M:%S'))
 log.addHandler(log_handler)
 log.setLevel(logging.INFO)
+log.info('***logger initialized')
 
-# input_var_1 = os.getenv('---')
-# input_var_2 = os.getenv('---')
-# input_var_3 = os.getenv('---')
+# NOTE: read env vars and set global vars
+virtual_cluster_id = os.getenv("EMR_VIRTUAL_CLUSTER_ID")
+log.info("***referencing virtual cluster id {0} via module {1}".format(virtual_cluster_id,
+                                                                os.path.basename(__file__)))
 
-# NOTE: check/log env vars
-log.info("***values of 1] ?={0} 2] ?={1} 3] ?={2}".format("??xxxxs?", "???", "???"))
+# TODO: add vars below
+S3_EMR_LOGS_BUCKET = os.getenv('LOGS_BUCKET')
+EMR_CONTAINER_LOGS_PATH = "s3://{0}/emr_containers/".format(S3_EMR_LOGS_BUCKET)
+SCRIPTS_BUCKET = os.getenv('S3_SCRIPT_BUCKET')
+ACOE_ACCOUNT_ID = os.getenv('ACOE_ACCOUNT_ID')
 
-# ---------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
 
-def default_event_pyspark_driver_config() -> dict:
+def default_emr_container_job_configuration_overrides() -> dict:
     """
-    *
+    *input payload for emr container start job
     """
+    # TODO: remove or provide conditional variable for this: "spark.jars.packages": "com.amazon.deequ:deequ:2.0.1-spark-3.2"
+    # TODO: add vars for this: spark.kubernetes.container.image
     return {
-            "jobDriver": 
+        "configurationOverrides": {
+            "applicationConfiguration": [
                 {
-                    "sparkSubmitJobDriver": {
-                    "entryPoint": "???",
-                    "entryPointArguments": None,
-                    "sparkSubmitParameters": """"--conf spark.sql.sources.partitionOverwriteMode=dynamic  \
-                                                --conf spark.executor.instances=1 \
-                                                --conf spark.executor.memory=3G
-                                                --conf spark.driver.memory=3G \
-                                                --conf spark.executor.cores=1 \
-                                                --conf spark.authenticate=false \
-                                                --conf spark.shuffle.service.enabled=false \
-                                                --conf spark.network.crypto.enabled=false \
-                                                --conf spark.authenticate.enableSaslEncryption=false"""
-                                            }
+                    "classification": "spark-env",
+                    "properties": {},
+                    "configurations": [
+                        {
+                            "classification": "export",
+                            "properties": {
+                                "PYSPARK_PYTHON": "/usr/bin/python3"
+                            },
+                            "configurations": []
+                        }
+                    ]
+                },
+                {
+                    "classification": "spark-defaults",
+                    "properties": {
+                        "spark.sql.execution.arrow.enabled": "true",
+                        "spark.memory.offHeap.enabled": "true",
+                        "spark.memory.offHeap.size": "5g",
+                        "spark.sql.debug.maxToStringFields": "100",
+                        "spark.ssl.enabled": "true",
+                        "spark.eventLog.enabled": "true",
+                        "spark.kubernetes.container.image": "{0}.dkr.ecr.us-east-1.amazonaws.com/foo-doo-emr-eks-spark-image:latest".format("xxx"),
+                        "spark.jars.packages": "org.json4s:json4s-jackson:3.2.11",
+                        "spark.speculation": "false"
+                    },
+                    "configurations": []
+                },
+                {
+                    "classification": "spark-hive-site",
+                    "properties": {
+                        "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",
+                        "hive.metastore.schema.verification": "false",
+                        "hive.exec.dynamic.partition": "true",
+                        "hive.exec.dynamic.partition.mode": "nonstrict",
+                        "spark.hadoop.hive.metastore.glue.catalogid": "{0}".format("xxx"),
+                        "spark.hadoop.fs.s3.maxRetries": "5"
+                    },
+                    "configurations": []
+                },
+                {
+                    "classification": "emrfs-site",
+                    "properties": {
+                        "fs.s3.useRequesterPaysHeader": "true",
+                        "fs.s3.maxRetries": "1"
+                    },
+                    "configurations": []
+                }
+            ],
+            "monitoringConfiguration": {
+                "persistentAppUI": "ENABLED",
+                "cloudWatchMonitoringConfiguration": {
+                    "logGroupName": "/emr-containers/jobs",
+                    "logStreamNamePrefix": "emr-continer-job-runs"
+                },
+                "s3MonitoringConfiguration": {
+                "logUri": "{0}".format("xxx")
                 }
             }
+        }
+    }
 
-def main(event_param, context_param=None):
-    return "what is this thing doing???"
+# def recursive_dict_key_value_search(key, iterable):
+#     """
+#     recursively search for a key in an iterable that may contain dictionaries and return its value.
+#     """
+#     if isinstance(iterable, dict):
+#         if key in iterable:
+#             return iterable[key]
+#         else:
+#             for value in iterable.values():
+#                 result = recursive_dict_key_value_search(key, value)
+#                 if result is not None:
+#                     return result
+#     elif isinstance(iterable, (list, tuple)):
+#         for item in iterable:
+#             result = recursive_dict_key_value_search(key, item)
+#             if result is not None:
+#                 return result
+#     return None
 
-# ---------------------------------------------------------------------------------------------
+def make_spark_submit_parameters():
+    sparkSubmitParameters = "--conf spark.sql.sources.partitionOverwriteMode=dynamic  --conf spark.executor.instances=1 \
+                                --conf spark.executor.memory=3G --conf spark.driver.memory=3G --conf spark.executor.cores=1 \
+                                --conf spark.authenticate=false --conf spark.shuffle.service.enabled=false --conf spark.network.crypto.enabled=false \
+                                --conf spark.authenticate.enableSaslEncryption=false"
+    return sparkSubmitParameters
 
-if __name__ == "__main__":
+def start_emr_container_job(event_param, context_param=None):
+    event_input['Input']['job_driver']['sparkSubmitJobDriver']['sparkSubmitParameters'] = spark_submit_parameters
+
+    emr_container_job_driver = recursive_dict_key_value_search("job_driver", event_input)
+    emr_container_job_configuration_overrides = recursive_dict_key_value_search("configurationOverrides", event_input)
+
+    try:
+        emrc_client = boto3.client('emr-containers')
+        log.debug("***emr-container client established with this setup: {0}".format(emrc_client))
+    except Exception as error:
+        log.error("***emr-container client failed to establish: {0}".format(error))
+        raise error
+    
+    start_job_run_response = emrc_client.start_job_run(
+                name=emr_container_job_name,
+                virtualClusterId=virtual_cluster_id,
+                clientToken=emr_container_job_uuid,
+                executionRoleArn=emr_container_job_iam_role,
+                releaseLabel=emr_container_release_label,
+                jobDriver=emr_container_job_driver,
+                configurationOverrides=emr_container_job_configuration_overrides
+    )
+
+    return start_job_run_response
+
+def main(event_arg, context_arg=None):
+
+    # NOTE: before running start_emr_container_job below, combine input args with default pyspark config overrides
+    event_arg = {**event_arg, **default_emr_container_job_configuration_overrides()}
+    return start_emr_container_job(event_arg, context_arg)
+
+# --------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
     main(event, context)
