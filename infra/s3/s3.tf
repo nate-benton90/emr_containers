21ccdd2d5bb5e438452e7fa2f8ffdbea55175502
nate-benton90 <nbenton90@gmail.com>
2025-01-01 11:27:27 -0600

    added s3 config changes to avoid tf warnings

--- infra/s3/s3.tf
+++ infra/s3/s3.tf
@@ -1,17 +1,19 @@
 // Resources
 resource "aws_s3_bucket" "emr-eks-scripts" {
-  bucket = "foo-doo-who-emr-container-scripts" // replace with your bucket name
-  acl    = "private"
-  versioning {
-    enabled = true
+  bucket = "foo-doo-who-emr-container-scripts" // Replace with your bucket name
+}
+
+resource "aws_s3_bucket_versioning" "emr_eks_scripts_versioning" {
+  bucket = aws_s3_bucket.emr-eks-scripts.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.emr-eks-scripts.bucket
   key    = "emr_container_job_template.py"
   source = "pyspark/emr_container_job_template.py"
-  acl    = "private"
 }
 
 // Outputs
